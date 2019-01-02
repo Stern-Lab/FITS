6f2e75adc5098d5bd7539460e545410a28a481cd
Tal Zinger <tal.zinger@outlook.com>
2019-01-02 12:59:16 +0200

    table header issues

--- fits/fits/ResultsStats_MutRate.cpp
+++ fits/fits/ResultsStats_MutRate.cpp
@@ -187,12 +187,12 @@ std::string ResultsStats::GetSummaryMutRate( bool table_only )
         ss << fits_constants::FILE_FIELD_DELIMITER;
         for (auto col=0; col<_num_alleles; ++col ) {
             ss << "to" + std::to_string(col)
-            << fits_constants::FILE_FIELD_DELIMITER
-            << fits_constants::FILE_FIELD_DELIMITER
-            << fits_constants::FILE_FIELD_DELIMITER;
+            << "-" << fits_constants::FILE_FIELD_DELIMITER
+            << "-" << fits_constants::FILE_FIELD_DELIMITER
+            << "-" << fits_constants::FILE_FIELD_DELIMITER;
             
             if ( col < _num_alleles-1 ) {
-                ss << fits_constants::FILE_FIELD_DELIMITER;
+                << "-" << fits_constants::FILE_FIELD_DELIMITER;
             }
         }
         ss << std::endl;
